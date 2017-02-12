70e7a56b820b6dc5a02dbe31086e7bd42712a02b
liyang <liyang85105@gmail.com>
2017-02-12 14:42:08 +0800

    remove main function in test_case implement file, use gtest_main library instead.

--- test/person_test.cpp
+++ test/person_test.cpp
@@ -6,14 +6,13 @@ TEST(Suite1, Case1)
     ASSERT_EQ(3.1415926, 3.1415926);
 }
 
+TEST(Suite1, Case2)
+{
+    ASSERT_EQ(3.1415926, 3.1415926);
+}
+
 TEST(Suite2, Case2)
 {
     Person p;
     ASSERT_EQ(42, p.id());
-}
-
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
